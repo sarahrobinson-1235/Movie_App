696e40713dd51d9c51fd0657270b1ca88f01874d
sarah <sarahaleesia1@gmail.com>
2021-01-26 00:48:32 -0600

    adds movie_id to actors and uses associatio

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,7 @@
 class Movie < ApplicationRecord
+  
+  has_many :actors
+  
   validates :title, uniqueness: true
   validates :plot, length: {in: 20..950}
   validates :year, numericality: {only_integer: true}
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-actor = Actor.create({first_name: "Taraji P.", last_name: "Henson", known_for: "Think Like A Man"})
+#Actor.create({first_name: "Taraji P.", last_name: "Henson", known_for: "Think Like A Man"})
 
-movie = Movie.create({title: "Two Can Play That Game", year: 2001, plot: "Shanté is so adept at navigating the waters of romance that her best girlfriends depend on her for advice whenever man trouble clouds the horizon. But when Shanté's boyfriend Keith (Morris Chestnut) is caught red-handed stepping out with her archrival Conny (Gabrielle Union), Shanté institutes her Ten Day Plan to get her man in line."})
+#Movie.create({title: "Two Can Play That Game", year: 2001, plot: "Shanté is so adept at navigating the waters of romance that her best girlfriends depend on her for advice whenever man trouble clouds the horizon. But when Shanté's boyfriend Keith (Morris Chestnut) is caught red-handed stepping out with her archrival Conny (Gabrielle Union), Shanté institutes her Ten Day Plan to get her man in line."})
+
