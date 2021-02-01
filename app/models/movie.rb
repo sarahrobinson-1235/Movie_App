9fbd6a1b0398dda31900c239179b552085786868
sarah <sarahaleesia1@gmail.com>
2021-01-31 23:04:28 -0600

    adds genre and moviegenre model. Adds many to many moviegenre asociations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,7 @@ class Movie < ApplicationRecord
   validates :title, uniqueness: true
   validates :plot, length: {in: 20..950}
   validates :year, numericality: {only_integer: true}
+
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
